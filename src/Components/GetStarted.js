1638e277ffe2b49f431b1fc7a4fb0bd631801244
mindbox_dev3 <manojkhatri.designs@gmail.com>
2020-03-06 20:03:49 +0530

    local commit

---
+++ src/Components/GetStarted.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class GetStarted extends Component {
+    render() {
+        return (
+            <section className="get-started">
+                <div className="container">
+                    <div className="lets-start">
+                        <div className="jumbotorn">
+                            <h1 className="main-title">Let’s make something amazing together.</h1>
+                            <a href="#" className="btn btn-underline">LET'S GET STARTED</a>
+                        </div>
+                        <div className="white-empty-overlay-left"></div>
+                    </div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default GetStarted;
